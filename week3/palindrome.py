28a76029ba28e1182d82bd4ed2400c8034b3ae4c
Thiago Viegas <thiagojoseviegas@gmail.com>
2025-10-20 18:24:39 -0400

    palindrome problem

---
+++ week3/palindrome.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        l, r = head, prev
+
+        while r:
+            if l.val != r.val:
+                return False
+
+            l = l.next
+            r = r.next
+
+        return True
+
+        
